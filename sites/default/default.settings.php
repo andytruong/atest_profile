a33cdb1215d0cb398cfae10a2976e79f05fc33f2
Andy Truong <andy@toila.net>
2014-01-03 11:37:42 +0700

    Update default.settings.php
    Disable mail sending

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -551,3 +551,6 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+// Disable mail sending
+$conf['mail_system']['default-system'] = 'TestingMailSystem';
