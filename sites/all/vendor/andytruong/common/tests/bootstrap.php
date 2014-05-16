e29ab54d6f8a4c47955e49f05f547eb5aacf2220
Andy Truong <andy@go1.com.au>
2014-05-16 15:43:46 +0700

    Hm

--- sites/all/vendor/andytruong/common/tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$loader = require __DIR__ . "/../vendor/autoload.php";
-$loader->addPsr4('AndyTruong\\Common\\TestCases\\', __DIR__.'/common');
--- sites/all/vendor/andytruong/render/tests/render/TestCase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AndyTruong\Render\TestCases;
-
-abstract class TestCase extends \PHPUnit_Framework_TestCase {
-  protected function getRender() {
-    return new \AndyTruong\Render\Render();
-  }
-}
